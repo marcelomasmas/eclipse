3ded83ea2a7bb36eaee19a637ba91053da28eac2
MARCELO <72521173+marcelomasmas@users.noreply.github.com>
2020-10-07 22:52:33 -0300

    adicionei arquivos via upload

---
+++ hello github/src/br/com/marcelo/git/helloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.marcelo.git;
+
+/** exemplo de integração do eclipse com o github*/
+
+
+public class helloGit {
+
+public static void main(String[] args) {
+	System.out.println("hello git");
+	
+	
+	
+	
+	
+	
+}
+
+}
