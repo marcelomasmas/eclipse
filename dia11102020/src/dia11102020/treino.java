7bf4e4b8a02652c7780444c2ac3edc5376c4b8e7
MARCELO <72521173+marcelomasmas@users.noreply.github.com>
2020-10-08 14:18:09 -0300

    Add files via upload

---
+++ dia11102020/src/dia11102020/treino.java
@@ -0,0 +1,42 @@
+package dia11102020;
+
+import java.util.Scanner;
+
+public class treino {
+
+	public static void main(String[] args) {{
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int x = 0;
+		int i = 0 ;
+		int y = 0 ;
+		int soma = 30;
+		
+		System.out.println("digite um numero");
+		
+		x = sc.nextInt();
+		
+		if (x > soma) {
+			
+			System.out.println("x e o maior numero");
+			
+		}
+		else {
+			System.out.println("valor invalido");
+			
+			
+			
+		}
+		
+		
+				
+		
+			
+			
+		}
+
+		
+	}
+
+}
