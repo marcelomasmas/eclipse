ac0fe1349a6fde8f73524db2dc72506af6234351
MARCELO <72521173+marcelomasmas@users.noreply.github.com>
2020-10-10 13:28:58 -0300

    Add files via upload
    treinos de java

---
+++ TreinoSabado/src/TreinoSabado/Treino.java
@@ -0,0 +1,42 @@
+package TreinoSabado;
+
+public class Treino {
+
+	public static void main(String[] args) {
+	
+    String original = "vdfaaaavfabcdbsdfb aaa FBFabcabcabcBGaaaBGRTG fr GFNaaaFGN";
+		
+	String s01 = original.toLowerCase();
+	String s02 =original.toUpperCase();
+	String s03 = original.trim();
+	String s04 =original.substring(2);
+	String s05 = original.substring(2,9);
+	String s06 = original.replace('a', 'x');
+	String s07 = original.replace("abc", "xy");
+	int i = original.indexOf("bc");
+	int j = original.lastIndexOf("bc");
+	
+	System.out.println("-" +original+ "-");
+	
+	System.out.println("tolowecase:" + s01);
+	
+	System.out.println("touppercase: -"+ s02 + "-");
+		
+	System.out.println("trim: -"+ s03 + "-");
+	
+	System.out.println("substring(2): -"+ s04 + "-");
+	
+	System.out.println("substring(2,9): -"+ s05 + "-");
+	
+	System.out.println("replace: -"+ s06 + "-");
+	
+	System.out.println("replace: -"+ s07 + "-");
+	
+	System.out.println("index of ''bc: -"+ s07 + i + "-");
+	
+	System.out.println("last index of 'bc': -"+ j + "-");
+	
+	
+	}
+
+}
