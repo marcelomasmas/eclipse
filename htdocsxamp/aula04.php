d3c14d5990053930b65beb12eb91894001f0da71
MARCELO <72521173+marcelomasmas@users.noreply.github.com>
2020-10-24 13:05:12 -0300

    Add files via upload

---
+++ htdocsxamp/aula04.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+ <meta charset ="utf-8/">
+
+ <title></title>
+
+</head>
+
+<body>
+
+<h1>ola mundo</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ htdocsxamp/olamundo.php
@@ -0,0 +1,39 @@
+<html>
+<head>   <title>formulario em php</title> </head>
+<body>
+
+<form action="" method="POST">
+
+<input type ="text"  name ="nome"  placeholder="digite seu nome">  <br> <br>
+
+<input type ="email"  name ="email"  placeholder="digite seu email"> <br> <br>
+
+<input type ="password"  name ="senha"  placeholder="digite sua senha"><br> <br>
+
+<input  type ="submit" value ="cadastro"><br> <br>
+
+
+
+
+
+
+
+</form>
+
+<?php
+
+echo "helo word" ;
+
+
+echo "estou aprendendo php"
+?>
+
+
+
+</body>
+</html>
+
+
+
+
+
